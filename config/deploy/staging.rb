f6d4a5581e80632f81ef2bbb8cb3e9abbaab750e
Soma Senga <senga.s.0801@gmail.com>
2019-11-22 12:14:31 +0900

    [add]自動デプロイ
    自動デプロイを実装しました。

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -59,13 +59,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-
-# server "db.example.com", user: "deploy", roles: %w{db}
-server "13.230.122.171/", user: "ec2-user", roles: %w{app db web}
-
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
-
-# role-based syntax
-# ==================

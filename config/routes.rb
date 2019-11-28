dff4f7b24dd35fdd028d31d4baf65d8ab3b6132a
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-11-28 12:13:29 +0900

    ビューの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :products
-  root to: "posts#index"
+  root to: "products#show"
 
 end

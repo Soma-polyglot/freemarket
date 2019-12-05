74379062453e428d98eba0186d39cf253ab69f90
shingomurakami59 <joyfut10shingo@gmail.com>
2019-12-05 16:00:13 +0900

    リストの追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -27,9 +27,24 @@ crumb :mypage_identification do |item|
   link '本人情報の登録', mypage_identification_path
   parent :mypage
 end
+
+
+crumb :mypage_credit do
+  link "支払い方法", mypage_card_index_path
+  parent :mypage
+end
+
+crumb :credit_card_information do
+  link "クレジットカード情報入力", new_mypage_card_path
+  parent :mypage
+end
+
+
 # -------------------- 商品関係 --------------------
 # 商品詳細
 crumb :item do |item|
   link "#{item.name}", item_path(item)
   parent :root
 end
+
+

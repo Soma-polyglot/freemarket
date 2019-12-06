3c7117b8cc74f47679ce8a7dc5c1e4c032e90d58
shingomurakami59 <joyfut10shingo@gmail.com>
2019-12-06 09:46:04 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :products
-  root to: "products#new"
+  resources :mypage
+  root to: "mypage#index"
 
 end

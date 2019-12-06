43220ac25ce4ddf4773de93635f4283ac3736c09
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-12-06 21:03:13 +0900

    productsアソシエーションにbelongs_to brand 追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
-  accepts_nested_attributes_for :product_image
-  accepts_nested_attributes_for :brand
+  # accepts_nested_attributes_for :product_image
+  # accepts_nested_attributes_for :brand
   has_many   :comments
   has_many   :users
   has_many   :product_images
   belongs_to :user
-  
+  belongs_to :brand
   belongs_to :category
   belongs_to :area
   belongs_to :size

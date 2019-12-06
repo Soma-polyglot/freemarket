eb44df13e3c76af090c2e5cad36d8bca9615ee8a
ema920 <yuki1994ema@gmail.com>
2019-12-06 12:27:41 +0900

    商品一覧表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.limit(10).order(created_at: :desc)
+    @redy = @products.eager_load(:category).where(id: 1)
+    @mens = @products.eager_load(:category).where(id: 2)
+    @camera = @products.eager_load(:category).where(id: 3)
+    @toys = @products.eager_load(:brand).where(id: 4)
+    @chanel = @products.eager_load(:brand).where(id: 1)
+    @vuitton = @products.eager_load(:brand).where(id: 2)
+    @supreme = @products.eager_load(:brand).where(id: 3)
+    @nike = @products.eager_load(:brand).where(id: 4)
   end
 
   def new

ebddcd1a1e71a88ff36caee471f1c304478476b8
shingomurakami59 <joyfut10shingo@gmail.com>
2019-12-09 19:08:58 +0900

    細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  root to: "users#index"
+  root to: "products#index"
+  # root to: "cards#edit"
   resources :users, only: [:edit,:show,:index]
   resources :products
   resources :mypage
 
-  root to: "products#index"
   resources :users, only: [:edit,:show] do
     collection do 
       get 'signout'
@@ -34,7 +33,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts
-  resources :cards, only: [:new, :create]
+  resources :cards, only: [:new, :create, :edit]
   resources :signup, only: [:create] do
     collection do
       get 'signup_link'
